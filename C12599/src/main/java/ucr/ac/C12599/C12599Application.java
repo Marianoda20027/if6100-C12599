be7a3da970dcf206945909910dd6b88e586e2baf
marianoda20027 <marianoda20027@gmail.com>
2024-08-16 19:56:09 -0600

    LAB1C12599

---
+++ C12599/src/main/java/ucr/ac/C12599/C12599Application.java
@@ -0,0 +1,13 @@
+package ucr.ac.C12599;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C12599Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C12599Application.class, args);
+	}
+
+}
